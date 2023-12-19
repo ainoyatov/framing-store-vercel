6f96a5930142f48752e10ae999304958e13397a0
Aki Inoyatov <info@gravityelements.com>
2023-12-19 13:20:07 -0700

    mobile ui adjustments

--- app/contact-us/page.tsx
+++ app/contact-us/page.tsx
@@ -6,13 +6,13 @@ const ContactPage = () => {
   return (
     <>
       <section>
-        <div className=''>
+        <div className='mb-16'>
           <FigmaContactForm />
         </div>
       </section>
       <section>
-        <div className='flex flex-col shadow-xl'>
-          <div className='w-auto xs:mt-[12rem] shadow-xl grid grid-cols-1 h-[40rem]'>
+        <div className='flex flex-col p-2 lg:p-0 mb-16'>
+          <div className='w-auto  xs:mt-[12rem] shadow-xl grid grid-cols-1 h-[40rem]'>
             <Maps />
           </div>
         </div>
