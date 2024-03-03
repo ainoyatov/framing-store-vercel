6c45147d3e18266de44ec69f6b4a664e87650f2e
Aki Inoyatov <info@gravityelements.com>
2024-03-03 08:29:12 -0700

    reviews max width added

--- app/page.tsx
+++ app/page.tsx
@@ -41,7 +41,7 @@ export default async function Home(){
         <SecondParagraph />
       </section>
 
-      <section className='w-[400px] xs:w-auto overflow-x-auto'>
+      <section className='w-[400px] xs:w-auto 2xl:max-w-[1600px] overflow-x-auto'>
         <div className='hero__title flex w-full justify-center my-16'>Recent Reviews</div>
         <div className='flex flex-row gap-4'>
           {reviews.map((review:any, index:number) => (
