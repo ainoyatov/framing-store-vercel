775c06a82251d06a74e2b5b697000aad920b8722
Aki Inoyatov <info@gravityelements.com>
2024-05-07 09:31:37 -0700

    testing CORS solution

--- app/frames/page.tsx
+++ app/frames/page.tsx
@@ -1,12 +1,28 @@
 
-import AllFrames from "@/components/mouldings/AllFrames";
+import searchMoldings from "@/lib/mouldings/defaultView";
+import NextImage from "next/image";
 
 
 export default async function MainFrame () {
 
+  const response = await searchMoldings();
+  const data = response.map((item:any) => item)
+  // console.log(id)
+
+
   return (
     <div className="py-8">
-      <AllFrames />
+      {data.map((item:any) => (
+        <div key={item.id}>
+          {item.description}
+          <NextImage 
+            src={`https://s3.amazonaws.com/im-dropbox-sync/${item.itemnum}.jpg`}
+            width={500}
+            height={500}
+            alt="prodoct images"
+          />
+        </div>
+      ))}
     </div>
   )
 }
