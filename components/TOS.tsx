578cc7e2cb6f8fb932e71062a692ac37024be6c3
Aki Inoyatov <info@gravityelements.com>
2024-06-28 11:53:24 -0700

    TOS margins updated

--- components/TOS.tsx
+++ components/TOS.tsx
@@ -19,7 +19,7 @@ export default function TOS() {
                     1. **Services Provided**
                 </div>
 
-                <div className="my-4 mx-2">
+                <div className="my-4">
                     We offer a variety of services, including but not limited to:
                 </div>
 
@@ -36,97 +36,97 @@ export default function TOS() {
                     <li>Jersey & Memorabilia Framing</li>
                 </ul>
 
-                <div className="my-8 mx-2">
+                <div className="my-8">
                     Specific terms and conditions may apply to each service, and such terms will be communicated to you at the time of service.
                 </div>
 
                 <div>
                     2. **Eligibility**
                 </div>
-                <div className="my-4 mx-2">
+                <div className="my-4">
                     To use our Services, you must be at least 18 years old or have the consent of a legal guardian. By using our Services, you represent and warrant that you meet these requirements.
                 </div>
 
                 <div>
                     3. **Orders and Payments**
                 </div>
-                <div className="my-4 mx-2">
+                <div className="my-4">
                     All orders placed with us are subject to acceptance. We reserve the right to refuse or cancel any order for any reason. Payment terms will be provided at the time of purchase. All prices are subject to change without notice.
                 </div>
 
                 <div>
                     4. **Custom Orders**
                 </div>
-                <div className="my-4 mx-2">
+                <div className="my-4">
                     For custom framing and art installation services, specific requirements will be discussed and agreed upon before the commencement of any work. Custom orders are final and non-refundable once work has begun.
                 </div>
                 <div>
                     5. **Shipping and Delivery**
                 </div>
-                <div className="my-4 mx-2">
+                <div className="my-4">
                     Shipping and delivery terms will vary based on the service and location. We will provide estimated delivery dates, but we are not responsible for delays beyond our control.
                 </div>
                 <div>
                     6. **Returns and Refunds**
                 </div>
-                <div className="my-4 mx-2">
+                <div className="my-4">
                     Returns and refunds are subject to our Return Policy. Please review our Return Policy for details on eligibility and the process for returns.
                 </div>
                 <div>
                     7. **Intellectual Property**
                 </div>
-                <div className="my-4 mx-2">
+                <div className="my-4">
                     All content, including but not limited to text, graphics, logos, images, and software, is the property of Art & Custom Frames or its licensors and is protected by applicable intellectual property laws. You may not use, reproduce, or distribute any content from our Services without our prior written permission.
                 </div>
                 <div>
                     8. **User Conduct**
                 </div>
-                <div className="my-4 mx-2">
+                <div className="my-4">
                     You agree not to use our Services for any unlawful purpose or in any way that could harm us or others. You also agree not to interfere with the operation of our Services or attempt to gain unauthorized access to our systems.
                 </div>
                 <div>
                     9. **Privacy**
                 </div>
-                <div className="my-4 mx-2">
+                <div className="my-4">
                     Your use of our Services is also governed by our Privacy Policy. Please review our Privacy Policy to understand our practices regarding your personal information.
                 </div>
                 <div>
                     10. **Disclaimers and Limitation of Liability**
                 </div>
-                <div className="my-4 mx-2">
+                <div className="my-4">
                     Our Services are provided "as is" and "as available." We make no warranties, express or implied, regarding the Services. To the maximum extent permitted by law, we disclaim all warranties and shall not be liable for any damages arising from your use of the Services.
                 </div>
                 <div>
                     11. **Indemnification**
                 </div>
-                <div className="my-4 mx-2">
+                <div className="my-4">
                     You agree to indemnify and hold us harmless from any claims, losses, liabilities, damages, and expenses, including attorney's fees, arising out of your use of our Services or any violation of these Terms.
                 </div>
                 <div>
                     12. **Governing Law**
                 </div>
-                <div className="my-4 mx-2">
+                <div className="my-4">
                     These Terms are governed by and construed in accordance with the laws of Arizona, without regard to its conflict of law principles. Any disputes arising under or in connection with these Terms shall be resolved in the courts of Arizona.
                 </div>
                 <div>
                     13. **Changes to These Terms**
                 </div>
-                <div className="my-4 mx-2">
+                <div className="my-4">
                     We reserve the right to modify these Terms at any time. We will notify you of any changes by posting the updated Terms on our website. Your continued use of the Services after any such changes constitutes your acceptance of the new Terms.
                 </div>
                 <div>
                     14. **Contact Us**
                 </div>
-                <div className="my-4 mx-2">
+                <div className="my-4">
                     If you have any questions about these Terms, please contact us at:
                 </div>
-                <div className="mx-2">
+                <div className="">
                     <div>Art & Custom Frames </div> 
                     <div>8221 East Evans Road, Suite C, Scottsdale, AZ 85260</div>  
                     <div>info@artandcustomframes.com </div>
                     <div>(480) 268-7182</div>
                 </div>
-                <div className="my-4 mx-2">
+                <div className="my-4">
                     Thank you for choosing Art & Custom Frames for your art and framing needs!
                 </div>       
             </div>
