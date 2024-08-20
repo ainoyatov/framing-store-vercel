0bf8552d3dbaafaf812341d801ef8b8a4bb9b906
Aki Inoyatov <info@gravityelements.com>
2024-08-20 12:19:58 -0700

    hours margin updated

--- app/store-hours/page.tsx
+++ app/store-hours/page.tsx
@@ -9,53 +9,9 @@ export const metadata: Metadata = {
 
 const page = () => {
   return (
-    <div className='flex flex-col lg:flex-row py-16 justify-between'>
+    <div className='flex flex-col sm:flex-row py-16 px-8 justify-between'>
       <div className='flex flex-col'>
-        <div className='flex justify-center sm:text-3xl lg:text-2xl'>Monday</div>
-        <div className='flex justify-center'>
-          <NextImage 
-            src='/weekday-working-hours.svg'
-            alt='Weekday hours of operation'
-            width={350}
-            height={30}
-          />
-        </div>
-      </div>
-      <div className='flex flex-col'>
-        <div className='flex justify-center sm:text-3xl lg:text-2xl'>Tuesday</div>
-        <div className='flex justify-center'>
-          <NextImage 
-            src='/weekday-working-hours.svg'
-            alt='Weekday hours of operation'
-            width={350}
-            height={30}
-          />
-        </div>
-      </div>
-      <div className='flex flex-col'>
-        <div className='flex justify-center sm:text-3xl lg:text-2xl'>Wednesday</div>
-        <div className='flex justify-center'>
-          <NextImage 
-            src='/weekday-working-hours.svg'
-            alt='Weekday hours of operation'
-            width={350}
-            height={30}
-          />
-        </div>
-      </div>
-      <div className='flex flex-col'>
-        <div className='flex justify-center sm:text-3xl lg:text-2xl'>Thursday</div>
-        <div className='flex justify-center'>
-          <NextImage 
-            src='/weekday-working-hours.svg'
-            alt='Weekday hours of operation'
-            width={350}
-            height={30}
-          />
-        </div>
-      </div>
-      <div className='flex flex-col'>
-        <div className='flex justify-center sm:text-3xl lg:text-2xl'>Friday</div>
+        <div className='flex justify-center text-3xl lg:text-4xl'>Mon - Fri</div>
         <div className='flex justify-center'>
           <NextImage 
             src='/weekday-working-hours.svg'
@@ -66,8 +22,9 @@ const page = () => {
         </div>
       </div>
       
+      
       <div className='flex flex-col'>
-        <div className='flex justify-center sm:text-3xl lg:text-2xl'>Saturday</div>
+        <div className='flex justify-center text-3xl lg:text-4xl'>Saturday</div>
         <div className='flex justify-center'>
           <NextImage 
             src='/weekend-working-hours.svg'
@@ -78,7 +35,7 @@ const page = () => {
         </div>
       </div>
       <div className='flex flex-col'>
-        <div className='flex justify-center sm:text-3xl lg:text-2xl'>Sunday</div>
+        <div className='flex justify-center text-3xl lg:text-4xl'>Sunday</div>
         <div className='flex justify-center'>
           <NextImage 
             src='/weekend-working-hours.svg'
