d2f021a215401f82983542b082c318ebdecd6643
Aki Inoyatov <info@gravityelements.com>
2024-08-20 14:42:41 -0700

    sunday design added

--- app/store-hours/page.tsx
+++ app/store-hours/page.tsx
@@ -38,7 +38,7 @@ const page = () => {
         <div className='flex justify-center text-3xl lg:text-4xl'>Sunday</div>
         <div className='flex justify-center'>
           <NextImage 
-            src='/weekend-working-hours.svg'
+            src='/sunday-work-hours.svg'
             alt='Sunday hours of operation'
             width={350}
             height={30}
