de6974248b493307b820f4d2d8d7c65755b5206e
Aki Inoyatov <info@gravityelements.com>
2024-09-12 15:50:18 -0700

    shop page bg updated again

--- app/shop/page.tsx
+++ app/shop/page.tsx
@@ -2,6 +2,8 @@ import EmblaCarousel from "@/components/embla/ui/EmblaCarousel";
 import TextCarousel from "@/components/navigation/textCarousel";
 import { framingCategories } from "@/constants";
 import Image from "next/image";
+import wallpaper from '@/public/wallpaper-framing.jpeg'
+
 
 const ShopPage = () => {
   
@@ -12,33 +14,34 @@ const ShopPage = () => {
   const categories = framingCategories.map((items:any) => items.links)
   const categoryText = categories[0]
 
-  
-  
-  
-
   return (
     <div>
-      
       <TextCarousel />
-      
-      <div className="flex p-4 px-8 pt-32 pb-8 bg-head-banner bg-center bg-cover justify-center md:justify-start lg:pl-24 ">
-        <div className="flex flex-col items-center p-6 lg:p-10 min-w-80 min-h-48 lg:w-[350px] lg:h-[250px] rounded-xl drop-shadow-xl bg-white">
-          <div className="text-3xl">70% off all</div>
-          <div className="text-3xl mb-2">In-house Frames</div>
-          <div className="mb-3">Guaranteed 2-3 day completion</div>
-          <div className="bg-emerald-700 rounded-full p-2 text-white">Call Now</div>
+  
+      <div className="flex w-full h-[1000px] p-4 px-8 pt-32 pb-8 bg-head-banner bg-center bg-cover justify-center items-center">
+        <div className="flex flex-col items-center p-8 w-full xs:max-w-[400px] bg-white rounded-3xl drop-shadow-2xl">
+          <div className="text-3xl">70% OFF</div>
+          <div className="text-2xl">In-Stock Frames</div>
+          <div className="m-2">Guaranteed 3-Day Turnaround !!</div>
+          <a href="tel:+14802687182">
+            <div className="bg-emerald-700 rounded-full p-2 text-white mt-2 drop-shadow-lg">Call Now</div>
+          </a>
+          
         </div>
       </div>
-
-      <div className="flex w-full  text-3xl lg:text-4xl py-16 justify-center">
+  
+      <div className="flex w-full  text-3xl lg:text-[50px] pt-16 lg:pt-32 justify-center">
         What are you framing?
       </div>
-
+  
       <div>
         <EmblaCarousel slides={categoryText} options={OPTIONS}/>
       </div>
+  
     </div>
   )
+
+
 }
 
 export default ShopPage;
--- tailwind.config.js
+++ tailwind.config.js
@@ -20,7 +20,7 @@ module.exports = {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-        'head-banner': "url('/pexels-bilakis.jpg')",
+        'head-banner': "url('/wallpaper-framing.jpeg')",
         
       },
 
