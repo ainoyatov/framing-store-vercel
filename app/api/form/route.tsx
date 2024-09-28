4b29cceb7c71a7ac6e31ad0b89d2ac1266986abb
Aki Inoyatov <info@gravityelements.com>
2024-09-28 16:38:09 -0700

    fixed err

--- app/api/form/route.tsx
+++ app/api/form/route.tsx
@@ -1,4 +1,4 @@
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(request:NextRequest) {
 
@@ -28,5 +28,5 @@ export async function POST(request:NextRequest) {
 
     }
 
-    return Response.json(res)
+    return NextResponse.json(res)
 }
