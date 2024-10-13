66a3865bf382fabfbffab6793598d1246a5dc37f
Aki Inoyatov <info@gravityelements.com>
2024-10-12 23:15:09 -0700

    dark mode update

--- app/shop/page.tsx
+++ app/shop/page.tsx
@@ -29,9 +29,9 @@ const ShopPage = () => {
   
       <div className="flex w-full h-[1000px] p-4 px-8 pt-32 pb-8 bg-head-banner bg-center bg-cover justify-center items-center">
         <div className="flex flex-col items-center p-8 w-full xs:max-w-[400px] bg-white rounded-3xl drop-shadow-2xl">
-          <div className="text-3xl">60% OFF</div>
-          <div className="text-2xl">In-Stock Frames</div>
-          <div className="m-2">Guaranteed 3-Day Turnaround !!</div>
+          <div className="text-3xl text-black">60% OFF</div>
+          <div className="text-2xl text-black">In-Stock Frames</div>
+          <div className="m-2 text-black">Guaranteed 3-Day Turnaround !!</div>
           <a href="tel:+14802687182">
             <div className="bg-emerald-700 rounded-full p-2 text-white mt-2 drop-shadow-lg">Call Now</div>
           </a>
