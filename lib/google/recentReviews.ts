153f48f913cfea0d7c925417db0d65867069de38
Aki Inoyatov <info@gravityelements.com>
2025-01-10 14:09:57 -0700

    google maps api update

--- lib/google/recentReviews.ts
+++ lib/google/recentReviews.ts
@@ -15,6 +15,7 @@ export default async function RecentReviews() {
                 body: JSON.stringify({})
             });
 
+
             if (!response.ok) {
                 throw new Error(`HTTP error! status: ${response.status}`);
             }
@@ -33,34 +34,3 @@ export default async function RecentReviews() {
 
     return reviews;
 }
-
-
-
-
-
-// export default async function RecentReviews () {
-
-//     const placeID = process.env.PLACE_ID!
-//     const apiKey = process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY!
-
-//     const url = `https://maps.googleapis.com/maps/api/place/details/json?place_id=${placeID}&fields=reviews&reviews_sort=newest&key=${apiKey}`
-
-//     // Get the most recent 5 reviews
-//     const getReviews = async () => {
-//         const response = await fetch(url, {
-//             method: 'POST',
-//             headers: {
-//                 'Content-Type': 'application/json'
-//             },
-//             body: JSON.stringify({})
-//         })
-
-//         return response.json()
-//     }
-
-//     const data = await getReviews();
-    
-//     const reviews = data.result.reviews.map((review:any) => review)
-
-//     return reviews;
-// }
--- lib/google/reviewTotalCount.ts
+++ lib/google/reviewTotalCount.ts
@@ -25,32 +25,3 @@ export default async function TotalReviewCount() {
 
     return reviewTotalnum;
 }
-
-
-
-// export default async function TotalReviewCount () {
-
-//     const placeID = process.env.PLACE_ID!
-//     const apiKey = process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY!
-
-//     const url = `https://maps.googleapis.com/maps/api/place/details/json?place_id=${placeID}&fields=user_ratings_total&key=${apiKey}`
-
-//     // Get the total number of reviews
-//     const reviewTotal = async () => {
-//         const response = await fetch(url, {
-//             method: 'POST',
-//             headers: {
-//                 'Content-Type': 'application/json'
-//             },
-//             body: JSON.stringify({})
-//         })
-
-//         return response.json()
-//     }
-
-//     const data = await reviewTotal()
-    
-//     const reviewTotalnum = data.result.user_ratings_total
-    
-//     return reviewTotalnum;
-// }
