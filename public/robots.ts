61438b77ce62c0a73152d536c66e48272a2270dd
Aki Inoyatov <info@gravityelements.com>
2025-03-10 16:37:20 -0700

    robots txt fixed

--- public/robots.ts
+++ public/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: '/',
       disallow: '/private/',
     },
-    sitemap: 'https://artandcustomframes.com/sitemap.ts',
+    sitemap: 'https://artandcustomframes.com/sitemap.txt',
   }
 }
