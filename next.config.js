54dd9c4b89120542ba0b55efd42e3d81cfc0c475
Aki Inoyatov <info@gravityelements.com>
2025-03-25 11:23:30 -0700

    next config redirects

--- next.config.js
+++ next.config.js
@@ -2,37 +2,92 @@
 
 module.exports = {
     images: {
-        
-        unoptimized: true,
-        formats: ['image/avif', 'image/webp'],
-        remotePatterns: [
+      unoptimized: true,
+      formats: ['image/avif', 'image/webp'],
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'images.pexels.com',
+          port: '',
+          pathname: '/photos/**'
+        },
+        {
+          protocol: 'https',
+          hostname: 'cdn.shopify.com',
+          port: '',
+          pathname: '/s/files/**'
+        },
+        {
+          protocol: 'https',
+          hostname: 'lh3.googleusercontent.com',
+          port: '',
+          pathname: '/a*/**'
+        },
+        {
+          protocol: 'https',
+          hostname: 's3.amazonaws.com',
+          port: '',
+        }
+      ]
+    },
+  
+    async redirects() {
+      return [
+        {
+          source: '/:path*',
+          has: [
             {
-                protocol: 'https',
-                hostname: 'images.pexels.com',
-                port: '',
-                pathname: '/photos/**'
+              type: 'host',
+              value: 'artandcustomframes.com',
             },
+          ],
+          destination: 'https://www.artandcustomframes.com/:path*',
+          permanent: true,
+        },
+        {
+          source: '/:path*',
+          has: [
             {
-                protocol: 'https',
-                hostname: 'cdn.shopify.com',
-                port: '',
-                pathname: '/s/files/**'
+              type: 'host',
+              value: 'customframing.store',
             },
+          ],
+          destination: 'https://www.artandcustomframes.com/:path*',
+          permanent: true,
+        },
+        {
+          source: '/:path*',
+          has: [
             {
-                protocol: 'https',
-                hostname: 'lh3.googleusercontent.com',
-                port: '',
-                pathname: '/a*/**'
+              type: 'host',
+              value: 'www.customframing.store',
             },
+          ],
+          destination: 'https://www.artandcustomframes.com/:path*',
+          permanent: true,
+        },
+        {
+          source: '/:path*',
+          has: [
             {
-                protocol: 'https',
-                hostname: 's3.amazonaws.com',
-                port: '',
-            }
-        ]
-    }
-}
-
-
-
-
+              type: 'host',
+              value: 'artframing.studio',
+            },
+          ],
+          destination: 'https://www.artandcustomframes.com/:path*',
+          permanent: true,
+        },
+        {
+          source: '/:path*',
+          has: [
+            {
+              type: 'host',
+              value: 'www.artframing.studio',
+            },
+          ],
+          destination: 'https://www.artandcustomframes.com/:path*',
+          permanent: true,
+        },
+      ];
+    },
+  };
