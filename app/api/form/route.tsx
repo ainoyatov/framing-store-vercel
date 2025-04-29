b9d1471e8f9c5de9eb66e0109413b7e25aa29f43
Aki Inoyatov <info@gravityelements.com>
2025-04-28 23:03:09 -0700

    email form updated

--- app/api/form/route.tsx
+++ app/api/form/route.tsx
@@ -54,37 +54,3 @@ export async function POST(request: NextRequest) {
     score,
   })
 }
-
-
-// import { NextRequest, NextResponse } from "next/server";
-
-// export async function POST(request: NextRequest) {
-//   // Parse JSON body instead of formData
-//   const data = await request.json();
-
-//   const name = data.name || '';
-//   const email = data.email || '';
-//   const phone = data.phone || '';
-//   const purpose = data.purpose || '';
-//   const message = data.message || '';
-
-//   // Form status
-//   const status = 'ok';
-//   const sent = true;
-//   const msg = 'Your message has been sent.';
-
-//   const res = {
-//     name,
-//     email,
-//     phone,
-//     purpose,
-//     message,
-//     status,
-//     sent,
-//     msg,
-//   };
-
-//   console.log(res);
-
-//   return NextResponse.json(res);
-// }
