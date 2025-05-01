9d53c766e2e935163beea0bb891b2a59eb3453b1
Aki Inoyatov <info@gravityelements.com>
2025-04-30 18:40:10 -0700

    clean up

--- app/opengraph-image.tsx
+++ app/opengraph-image.tsx
@@ -31,12 +31,3 @@ export default async function Image() {
     size
   );
 }
-
-
-// import OpengraphImage from '@/components/shopify/utilities/opengraph-image';
-
-// export const runtime = 'edge';
-
-// export default async function Image() {
-//   return await OpengraphImage();
-// }
